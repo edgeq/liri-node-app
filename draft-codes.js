2b049fabdfa617834a18f64065dc368644c42a6a
Edge Quintanilla <edgarq86@gmail.com>
2017-10-13 14:40:16 -0500

    tweets, spotify, movies, and fsRead work

--- draft-codes.js
+++ draft-codes.js
@@ -1,17 +1,17 @@
-// switch (itermInput){
-//   case inputArray[0]:
-//   console.log("you tweeted...");
-//   break;
-//   case inputArray[1]:
-//   console.log("spotify song...");
-//   break;
-//   case inputArray[2]:
-//   console.log("OMDB pull...");
-//   break;
-//   case inputArray[3]:
-//   console.log("run text file...");
-//   break;
-//   default:
-//   console.log("Please use one of the array options")
-// }
+switch (iTermInput){
+  case inputArray[0]:
+  console.log("you tweeted...");
+  break;
+  case inputArray[1]:
+  console.log("spotify song...");
+  break;
+  case inputArray[2]:
+  console.log("OMDB pull...");
+  break;
+  case inputArray[3]:
+  console.log("run text file...");
+  break;
+  default:
+  console.log("Please use one of the array options")
+}
 // }
--- liri.js
+++ liri.js
@@ -1,28 +1,18 @@
 //At the top of the `liri.js` file, write the code you need to grab the data from keys.js.
 var request = require('request');
 var keys = require('./keys.js');
+var fs = require('fs');
 // console.log(twitter);
 //Then store the keys in a variable.
 var twitterKeys = keys.twitterKeys;
 var spotifyKeys = keys.spotifyKeys;
- //console.log(twitterKeys); returns an object
+var iTermInput = process.argv[2]
+var userKey = process.argv.splice(3).join(" ");
 
-// individual keys
-// var twitterKey = twitter.consumer_key;
-// console.log("twitter key: " + twitterKey);
-// var twitterSecret = twitter.consumer_secret;
-// console.log("twitter secret: " + twitterSecret);
-// var twitterAccessToken = twitter.access_token_key;
-// console.log("twitter access token: " + twitterAccessToken);
-// var twitterTokenSecret = twitter.access_token_secret;
-// console.log("twitter acess token secret: " + twitterTokenSecret);
-
-//===================//
 
 //8. Make it so liri.js can take in one of the following commands:
   // * `my-tweets` * `spotify-this-song` * `movie-this` * `do-what-it-says`
 
-  var iTermInput = process.argv[2]
   var inputArray = ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"];
    //What each command should do:
    /*if (iTermInput[0]){
@@ -30,8 +20,7 @@ var spotifyKeys = keys.spotifyKeys;
           Twitter NPM documentation for searching tweets
         console.log(show when created);
       }*/
-
-      if (iTermInput === inputArray[0]){
+      if (iTermInput === inputArray[0]) {
         var Twitter = require('twitter');
         var client = new Twitter(twitterKeys);
         var params = {screen_name: 'edgeriffs'};
@@ -50,18 +39,13 @@ var spotifyKeys = keys.spotifyKeys;
 
       } else if(iTermInput === inputArray[1]){
 
-
-        var spotifySong = process.argv[3];
         var Spotify = require('node-spotify-api');
 
         var spotify = new Spotify(spotifyKeys);
 
-        spotify.search({ type: 'track', query: spotifySong, limit: 1}, function(err, data) {
+        spotify.search({ type: 'track', query: userKey, limit: 1}, function(err, data) {
           if (err) {
             return console.log('Error occurred: ' + err);
-          }
-          if (spotifySong == ''){
-            console.log('put something')
           }
           /*conosle.log(Artist) * console.log(song's name) * conzole.log(song preview link) * console.log(album) */
           console.log(data.tracks.items[0].artists[0].name);
@@ -74,17 +58,40 @@ var spotifyKeys = keys.spotifyKeys;
 
         console.log("spotify song...");
       } else if(iTermInput === inputArray[2]){
-        /*
-        else if (iTermInput[2]) {
-          console.log(Title of Movie / Year / RatingIMDB / RatingRT / Country / Language / Plot / Actors);
+        console.log(userKey, " this is userKey");// userKey = process.argv[3];console
+        request("http://www.omdbapi.com/?t=" + userKey + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
+          // If the request is successful (i.e. if the response status code is 200)
+          if (!error && response.statusCode === 200) {
+         /*
+         else if (iTermInput[2]) {
+         console.log(Title of Movie / Year / RatingIMDB / RatingRT / Country / Language / Plot / Actors);
+         */
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("==============");
+          }
+        });
+/*
           if (no movie){
             Mr. Nobody
           }
         }*/
         console.log("OMDB pull...");
       } else if (iTermInput === inputArray[3]){
+        fs.readFile("random.txt", "utf8", function(err, data){
+          //log contents of file into process.argv
+          var output = data.split(",").join(" ").split("\"").join(" ");
+          console.log(output);
+        })
         console.log("run text file...");
+
       } else {
         console.log("please use a valid command")
       };
